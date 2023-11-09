b87e5cccf6d93709644a399313baf7bba4c66545
Znis Twayana <metwayana@gmail.com>
2023-11-09 12:49:06 +0545

    links updated it2 nov9

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1136,7 +1136,7 @@ export const RoutingLinks = {
   contact: "/personal-portfolio/contact",
   resume: "resume.pdf",
   ghub: "https://github.com/Znis",
-  linkedin: "www.linkedin.com/in/jenish-twayana",
+  linkedin: "https://linkedin.com/in/jenish-twayana",
   twitter: "https://twitter.com/zentw11",
   mail: "mailto:metwayana@gmail.com",
 };
