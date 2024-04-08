a7a9ab823d51eb6ff486084f11e7f1ab12ab3efe
Znis Twayana <metwayana@gmail.com>
2024-04-08 12:21:55 +0545

    added new project minor fix april 8

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1474,7 +1474,7 @@ export function TechnologyList() {
           imgSrc: "bucket-icon.png",
           imgAlt: "Bucket Storage",
           imgTitle: "Bucket Storage",
-          isTechStack: true,
+          isTechStack: false,
         },
        
         
